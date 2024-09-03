83c3b0254598feb8e31954de78b003d8fe560692
somtsho <179671017+spx77-e59@users.noreply.github.com>
2024-09-03 15:29:19 +0600

    Add function for computer choice

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("yo");
+
+function getComputerChoice () {
+    const random = Math.random();
+    if (random<0.34) {
+        return "rock";
+    } else if(random<0.64) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
